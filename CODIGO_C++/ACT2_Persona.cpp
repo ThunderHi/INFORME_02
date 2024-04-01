edc1d49c69d13b7ecb7030aec8aaa4190ffe5d93
Nylder Baca <164599631+RipNylder10@users.noreply.github.com>
2024-04-01 18:44:21 -0500

    Update ACT2_Persona.cpp

--- CODIGO_C++/ACT2_Persona.cpp
+++ CODIGO_C++/ACT2_Persona.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 int main() {
-    Persona persona1("Juan", 30, "Masculino");
-    Persona persona2("Maria", 25, "Femenino");
+    //Creacion de los objetos
+    Persona persona1("Jose luis", 30, "Masculino");
+    Persona persona2("Marile", 25, "Femenino");
 
+    //Mostrar datos persona 1
     cout<<"Datos de la Persona 1:"<<endl;
     cout<<"Nombre: "<<persona1.getNombre()<<endl;
     cout<<"Edad: "<<persona1.getEdad()<<" years"<<endl;
     cout<<"Genero: "<<persona1.getGenero()<<endl;
 
+    //Mostrar datos persona 2
     cout<<"\nDatos de la Persona 2:"<<endl;
     cout<<"Nombre: "<<persona2.getNombre()<<endl;
     cout<<"Edad: "<<persona2.getEdad()<<" years"<<endl;

3cf083f1424b0204eea1de26eafd724859eabdb0
AngelSalasZapata <164601086+AngelSalasZapata@users.noreply.github.com>
2024-04-04 14:39:42 -0500

    Update ACT1_animales.py

--- CODIGO_PYTHON/ACT1_animales.py
+++ CODIGO_PYTHON/ACT1_animales.py
@@ -10,31 +10,31 @@ class Animal:
     def alimentar(self):
         print(self.nombre, "está siendo alimentado.")
 
-    #Método para hacer un sonido
+    #Motodo para hacer un sonido
     def hacer_sonido(self):
         print(self.nombre, "hace un sonido característico.")
 
-    #Método para mostrar la edad que tiene el animal
+    #Motodo para mostrar la edad que tiene el animal
     def mostrar_edad(self):
         print(self.nombre, "tiene", self.edad, "años.")
 
 
-#Creación de objetos de la clase Animal
+#Creacion de objetos de la clase Animal
 perro = Animal("Puchito", 7, "Perro")
 gato = Animal("Dieguito", 4, "Gato")
 pajaro = Animal("Piquito", 2, "Pájaro")
 
-#Interacción con el perro
+#Interaccion con el perro
 perro.alimentar()
 perro.hacer_sonido()
 perro.mostrar_edad()
 
-#Interacción con el gato
+#Interaccion con el gato
 gato.alimentar()
 gato.hacer_sonido()
 gato.mostrar_edad()
 
-#Interacción con el pájaro
+#Interaccion con el pájaro
 pajaro.alimentar()
 pajaro.hacer_sonido()
 pajaro.mostrar_edad()

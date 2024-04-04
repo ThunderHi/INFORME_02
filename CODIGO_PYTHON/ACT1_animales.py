19cdbdb1584d8b4d3144258b844497a43b106566
Nylder Baca <164599631+RipNylder10@users.noreply.github.com>
2024-04-04 14:31:57 -0500

    Update ACT1_animales.py

--- CODIGO_PYTHON/ACT1_animales.py
+++ CODIGO_PYTHON/ACT1_animales.py
@@ -1,40 +1,40 @@
-# Definimos la clase Animal
+#Definimos la clase Animal
 class Animal:
-    # Constructor de la clase Animal que inicializa con los atributos
+    #Constructor de la clase Animal que inicializa con los atributos
     def __init__(self, nombre, edad, tipo):
         self.nombre = nombre
         self.edad = edad
         self.tipo = tipo
 
-    # Método para alimentar
+    #Método para alimentar
     def alimentar(self):
         print(self.nombre, "está siendo alimentado.")
 
-    # Método para hacer un sonido
+    #Método para hacer un sonido
     def hacer_sonido(self):
         print(self.nombre, "hace un sonido característico.")
 
-    # Método para mostrar la edad que tiene el animal
+    #Método para mostrar la edad que tiene el animal
     def mostrar_edad(self):
         print(self.nombre, "tiene", self.edad, "años.")
 
 
-# Creación de objetos de la clase Animal
+#Creación de objetos de la clase Animal
 perro = Animal("Puchito", 7, "Perro")
 gato = Animal("Dieguito", 4, "Gato")
 pajaro = Animal("Piquito", 2, "Pájaro")
 
-# Interacción con el perro
+#Interacción con el perro
 perro.alimentar()
 perro.hacer_sonido()
 perro.mostrar_edad()
 
-# Interacción con el gato
+#Interacción con el gato
 gato.alimentar()
 gato.hacer_sonido()
 gato.mostrar_edad()
 
-# Interacción con el pájaro
+#Interacción con el pájaro
 pajaro.alimentar()
 pajaro.hacer_sonido()
 pajaro.mostrar_edad()

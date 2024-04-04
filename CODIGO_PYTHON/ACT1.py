d5b1f4f9c9c16c7c3adf49ad532348a0c1606c19
Nylder Baca <164599631+RipNylder10@users.noreply.github.com>
2024-04-04 14:29:52 -0500

    Update ACT1.py

--- CODIGO_PYTHON/ACT1.py
+++ CODIGO_PYTHON/ACT1.py
@@ -1,3 +1,40 @@
-print("holadmlkanansa")
-print("ahora si creo que ya")
-print("hola thunder")
+# Definimos la clase Animal
+class Animal:
+    # Constructor de la clase Animal que inicializa con los atributos
+    def __init__(self, nombre, edad, tipo):
+        self.nombre = nombre
+        self.edad = edad
+        self.tipo = tipo
+
+    # Método para alimentar
+    def alimentar(self):
+        print(self.nombre, "está siendo alimentado.")
+
+    # Método para hacer un sonido
+    def hacer_sonido(self):
+        print(self.nombre, "hace un sonido característico.")
+
+    # Método para mostrar la edad que tiene el animal
+    def mostrar_edad(self):
+        print(self.nombre, "tiene", self.edad, "años.")
+
+
+# Creación de objetos de la clase Animal
+perro = Animal("Puchito", 7, "Perro")
+gato = Animal("Dieguito", 4, "Gato")
+pajaro = Animal("Piquito", 2, "Pájaro")
+
+# Interacción con el perro
+perro.alimentar()
+perro.hacer_sonido()
+perro.mostrar_edad()
+
+# Interacción con el gato
+gato.alimentar()
+gato.hacer_sonido()
+gato.mostrar_edad()
+
+# Interacción con el pájaro
+pajaro.alimentar()
+pajaro.hacer_sonido()
+pajaro.mostrar_edad()

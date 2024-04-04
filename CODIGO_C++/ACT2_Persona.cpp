b12d7515285b5f56bf0016012b0e68aaa0791feb
Nylder Baca <164599631+RipNylder10@users.noreply.github.com>
2024-04-04 15:51:43 -0500

    Update ACT2_Persona.cpp

--- CODIGO_C++/ACT2_Persona.cpp
+++ CODIGO_C++/ACT2_Persona.cpp
@@ -49,16 +49,16 @@ int main() {
     Persona persona2("Maria", 25, "Femenino");
 
     //Imprimimos los datos de la Persona 1
-    cout << "Datos de la Persona 1:" << endl;
-    cout << "Nombre: " << persona1.getNombre() << endl;
-    cout << "Edad: " << persona1.getEdad() << " years" << endl;
-    cout << "Genero: " << persona1.getGenero() << endl;
+    cout<<"Datos de la Persona 1:"<<endl;
+    cout<<"Nombre: "<<persona1.getNombre()<<endl;
+    cout<<"Edad: "<<persona1.getEdad()<<" years"<<endl;
+    cout<<"Genero: "<<persona1.getGenero()<<endl;
 
     //Imprimimos los datos de la Persona 2
-    cout << "\nDatos de la Persona 2:" << endl;
-    cout << "Nombre: " << persona2.getNombre() << endl;
-    cout << "Edad: " << persona2.getEdad() << " years" << endl;
-    cout << "Género: " << persona2.getGenero() << endl;
+    cout<<"\nDatos de la Persona 2:" << endl;
+    cout<<"Nombre: "<<persona2.getNombre()<<endl;
+    cout<<"Edad: "<<persona2.getEdad()<<" years"<<endl;
+    cout<<"Género: "<<persona2.getGenero()<<endl;
 
     return 0;
 }
